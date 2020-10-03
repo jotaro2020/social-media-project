ef7743d0f411889e1b386119e7e06b5ca31a34d4
Zhassulan Abishev <68692894+jotaro2020@users.noreply.github.com>
2020-10-03 12:43:06 +0600

    Update util.config.js

--- functions/util/util.config.js
+++ functions/util/util.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-        apiKey: "AIzaSyDilSseEZ_na3V1KoeOwsDW5wYscbHLv8M",
+        apiKey: "",
         authDomain: "social-media-project-spartan.firebaseapp.com",
         databaseURL: "https://social-media-project-spartan.firebaseio.com",
         projectId: "social-media-project-spartan",
         storageBucket: "social-media-project-spartan.appspot.com",
-        messagingSenderId: "1074378204405",
-        appId: "1:1074378204405:web:4c3a428d7489a8f02fd1bc",
-        measurementId: "G-GJJ9TE07LN",
+        messagingSenderId: "",
+        appId: "1::web:",
+        measurementId: "G-",
 }
